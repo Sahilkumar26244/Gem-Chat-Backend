bd3908244093889b4f104386bdddc684a359f4ea
Sahil Kumar <kumarsahil8981@gmail.com>
2025-01-30 15:23:37 +0530

    user services is created now

--- models/user.model.js
+++ models/user.model.js
@@ -29,3 +29,7 @@ userSchema.methods.isValidPassword = async function (password) {
 userSchema.methods.generateJWT = function () {
     return jwt.sign({email:this.email}, process.env.JWT_SECRET)
 };
+
+const User = mongoose.model('user', userSchema);
+
+export default User;
