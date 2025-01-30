81b48e92163c78374f7bd6efb146a16b817fbd16
Sahil Kumar <kumarsahil8981@gmail.com>
2025-01-30 15:16:06 +0530

    db is connected and user model is ready to config

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import morgan from "morgan";
+import connect from "./db/db.js";
+
+connect()
 
 const app = express();
 
-
+app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
---
+++ db/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+function connect(){
+    mongoose.connect(process.env.MONGO_URL).then(() => {
+        console.log("Connect to MongoDB")
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+export default connect
