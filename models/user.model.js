a8cc2a434312e3c0eb4d2268d0c5fae7b41d2f36
Sahil Kumar <kumarsahil8981@gmail.com>
2025-02-10 14:51:46 +0530

    project create route with function is created

--- models/user.model.js
+++ models/user.model.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true,
+    // unique: true,
     trim: true,
     lowercase: true,
     minLength: [6, "Email must be at least 6 characters long."],
